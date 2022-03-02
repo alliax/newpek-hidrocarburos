4e3f3687592d381395e48c731e3e01134a5cacb9
Eduardo Roth <eduardo@eduardoroth.dev>
2022-03-01 23:56:05 -0600

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hidrocarburos-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
