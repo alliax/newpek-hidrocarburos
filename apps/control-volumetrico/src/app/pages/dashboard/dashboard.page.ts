4cb4a6a4b7cce662cc9598a9f7f7d9826b834c4a
danielMartinezCarrillo <danielmtz900@gmail.com>
2022-08-16 14:40:51 -0500

    Proyecto Newpek (JSON)

---
+++ apps/control-volumetrico/src/app/pages/dashboard/dashboard.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hidrocarburos-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+})
+export class DashboardPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
